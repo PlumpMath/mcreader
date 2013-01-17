629284ca5e8e4df88197806e31677aeee04ea219
Peter Meinhold <peterm@deepspace.ucsb.edu>
2013-01-17 15:52:23 -0800

    first commit

---
+++ converter.py
@@ -0,0 +1,84 @@
+import os
+import datetime as dt
+import numpy as np
+import h5py
+import getData
+import time
+
+	
+def bcd_to_int(bcd_str):
+	string= ''
+	return int(string.join([str(int(bcd_str[0:2],2)), str(int(bcd_str[2:6],2)), str(int(bcd_str[6:10],2)), str(int(bcd_str[10:14],2)), str(int(bcd_str[14:18],2))]))
+	
+def bin_to_int(bin_str):
+	return int(bin_str,2)
+
+def fileStruct(n_array):
+	
+	t=dt.datetime.now()
+	date = t.strftime("%m-%d-%Y")
+	time = t.strftime("%H-%M")
+	if not os.path.exists(date):#this is the first file being created for that time
+		os.makedirs(date)
+	
+	path = '/'.join((date,time))
+	path = '.'.join((path,"h5"))
+	print(path)
+	#fn = os.path.join(os.path.dirname(__file__), 'my_file')
+	#Sprint(fn)
+	
+	with h5py.File(str(path)) as h5file:
+		h5file.create_dataset("data", data=n_array)
+		#set = h5file.create_dataset("data", (100, 100), 'i')
+	
+	
+
+class datacollector(object):
+	def __init__(self,size):
+		self.index = 0
+		self.data = np.zeros(size, dtype=[("az", np.int), ("el", np.int), ("time", np.int)])
+	def add(self,az,el,time):
+		self.data[self.index] = ((az,el,time))
+		self.index += 1
+	def getData(self):
+		return self.data
+	
+	
+if __name__=='__main__':
+	'''print(bcd_to_int('101001000000000001'))
+	print(bin_to_int('0011'))
+	all = getData.getData()
+	all = (bcd_to_int(all[0]), bin_to_int(all[1]), bin_to_int(all[2]))
+	data = np.zeros(1000, dtype=[("first", np.int), ("second", np.int), ("third", np.int)])
+	data[1]=all
+	fileStruct(data)
+	'''
+	data = np.zeros(1000, dtype=[("first", np.int), ("second", np.int)])
+	eye = getData.Eyeball()
+	Data = datacollector(1000)
+	fileStruct(Data.getData())
+
+	
+	while True:
+		#time_a = time.time()
+		
+		Data = datacollector(1000)
+		
+		#timer loop
+		all = eye.getData()
+		
+		all = (bcd_to_int(all[0]), bin_to_int(all[1]), bin_to_int(all[2]))
+		print all
+		Data.add(all[0],all[1],all[2])
+		#fileStruct(Data.getData())
+		
+		
+		
+		#time_b = time.time()
+		#delta = time_b-time_a
+		
+		#print delta
+		
+		#time.sleep(.03-delta)
+		
+		
---
+++ getData.py
@@ -0,0 +1,68 @@
+from PyDAQmx import *
+from PyDAQmx.DAQmxCallBack import *
+from numpy import zeros
+
+
+
+class Eyeball(object):
+	def __init__(self):
+		self.data = numpy.zeros((97,), dtype=numpy.uint8)
+		self.i = 0
+		self.err=""
+		self.read, self.bytesPerSamp=int32(), int32()
+		DAQmxResetDevice('dev1')
+		self.taskHandle=TaskHandle()
+		
+		DAQmxCreateTask("",byref(self.taskHandle))
+		
+
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port4","",DAQmx_Val_ChanForAllLines)
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port7","",DAQmx_Val_ChanForAllLines)
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port10","",DAQmx_Val_ChanForAllLines)
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port2","",DAQmx_Val_ChanForAllLines)
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port5","",DAQmx_Val_ChanForAllLines)
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port0","",DAQmx_Val_ChanForAllLines)
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port3","",DAQmx_Val_ChanForAllLines)
+		DAQmxCreateDIChan(self.taskHandle,"Dev1/port9","",DAQmx_Val_ChanForAllLines)
+		
+		DAQmxStartTask(self.taskHandle)
+
+	def close(self):
+		print "bye"
+		
+		DAQmxStopTask(self.taskHandle)
+		DAQmxClearTask(self.taskHandle)
+
+
+	def __del__(self):
+		#self.close()
+		pass
+
+	def getData(self):
+		
+		DAQmxReadDigitalLines(self.taskHandle,1,10.0,DAQmx_Val_GroupByChannel,self.data,100,byref(self.read),byref(self.bytesPerSamp),None)
+		index=0
+		"""
+		for each in self.data:
+			
+			print str(each)+":"+str(index)
+			index=index+1
+			"""
+		all = [self.data[d] for d in range(0,95)]
+		all = map(str, all)
+		a
+		return  [''.join(all[0:18]), ''.join((all[24:32])[::-1])+"".join((all[32:40])[::-1]), ''.join(all[41:65])]
+
+	
+if __name__=='__main__':
+	t = Eyeball()
+	#t.getData()
+	print t.getData()
+
+
+
+		
+
+		
+		
+		
